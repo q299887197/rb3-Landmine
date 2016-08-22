926246f53d02485fbaea8301d93a8ac069c30e69
bob_chen <q29887197@gmail.com>
2016-08-22 08:00:57 +0000

    Landmine4

--- Landmine4.php
+++ Landmine4.php
@@ -3,7 +3,7 @@
 $high = 10;
 $width = 10;
 $landmine = 40; //炸彈數
-$landmineNumber = 0; //周圍炸彈數量預設0
+
 
 $arr = array();
 
@@ -14,17 +14,17 @@ for($i=0; $i<$high; $i++)  //寫出方塊
 	}
 }
 
-for($i=0; $i<$landmine; $i++) //炸彈亂數
+for($i=1; $i <= $landmine; $i++) //炸彈亂數
 {
 	$x = rand(0, 9);  //座標亂數
 	$y = rand(0, 9);
 
-	for($j=0; $j<$i; $j++) //檢查重複
+	for($j=0; $j<=$i; $j++) //檢查重複
 	{
 		if ($x == $a[$j] && $y == $b[$j]) {  //座標重新亂數
 			$x = rand(0, 9);
 			$y = rand(0, 9);
-			$j=0;
+			$j = 0;
 		}
 	}
 
@@ -38,30 +38,32 @@ for($i=0; $i<$high; $i++)  //設定數字
 {
 	for($j=0; $j<$width; $j++){
 
-		if (!$arr[$i][$j] == "M") {
+		if ($arr[$i][$j] != "M") {
 
-			if ($arr[$i][$j+1] == "M") {  //上
+			$landmineNumber = '0'; //周圍炸彈數量預設0
+
+			if ($arr[$i][$j+1] === "M") {  //上
 				$landmineNumber++;
 			}
-			if ($arr[$i+1][$j+1] == "M") {  //右上
+			if ($arr[$i+1][$j+1] === "M") {  //右下
 				$landmineNumber++;
 			}
-			if ($arr[$i+1][$j] == "M") {  //右
+			if ($arr[$i+1][$j] === "M") {  //右
 				$landmineNumber++;
 			}
-			if ($arr[$i+1][$j-1] == "M") {  //右下
+			if ($arr[$i+1][$j-1] === "M") {  //右上
 				$landmineNumber++;
 			}
-			if ($arr[$i][$j-1] == "M") {  //下
+			if ($arr[$i][$j-1] === "M") {  //下
 				$landmineNumber++;
 			}
-			if ($arr[$i-1][$j-1] == "M") {  //左下
+			if ($arr[$i-1][$j-1] === "M") {  //左上
 				$landmineNumber++;
 			}
-			if ($arr[$i-1][$j] == "M") {  //左
+			if ($arr[$i-1][$j] === "M") {  //左
 				$landmineNumber++;
 			}
-			if ($arr[$i-1][$j+1] == "M") {  //左上
+			if ($arr[$i-1][$j+1] === "M") {  //左下
 				$landmineNumber++;
 			}
 
@@ -70,10 +72,11 @@ for($i=0; $i<$high; $i++)  //設定數字
 
 		$landmineNumber = '0'; //歸零
 
-		echo $arr[$i][$j];
+		echo $arr[$i][$j] . " ";
 	}
 
 	if ($i != $high-1) {  //最後不給N
 		echo "N";
 	}
+	echo "<BR>";
 }
